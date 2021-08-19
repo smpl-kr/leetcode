0755e8c1fc333d44c4046e40c5c84a6023bb20e0
js <c86j224s@gmail.com>
2021-08-20 01:17:38 +0900

    solve remove_duplicates_from_sorted_list.rs

--- src/remove_duplicates_from_sorted_list.rs
+++ src/remove_duplicates_from_sorted_list.rs
@@ -1,5 +1,5 @@
 // https://leetcode.com/problems/remove-duplicates-from-sorted-list/
-// todo
+// Solved
 
 //Definition for singly-linked list.
 #[derive(PartialEq, Eq, Clone, Debug)]
@@ -31,24 +31,88 @@ impl ListNode {
     }
 }
 
+// recursive approach
 pub(self) mod first_try {
     use crate::remove_duplicates_from_sorted_list::ListNode;
 
     pub struct Solution;
     impl Solution {
+        fn rec(prev_val: i32, cur: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+            if cur.is_none() {
+                return None
+            }
+
+            let mut cur = cur.unwrap();
+            if prev_val == cur.val {
+                return Self::rec(prev_val, cur.next)
+            }
+
+            cur.next = Self::rec(cur.val, cur.next);
+            Some(cur)
+        }
+
         pub fn delete_duplicates(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-            todo!()
+            if head.is_none() {
+                return None
+            }
+
+            let mut head = head.unwrap();
+            head.next = Self::rec(head.val, head.next);
+            Some(head)
+        }
+    }
+}
+
+// procedural approach
+pub(self) mod second_try {
+    use crate::remove_duplicates_from_sorted_list::ListNode;
+    pub struct Solution;
+    impl Solution {
+        pub fn delete_duplicates(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+            if head.is_none() {
+                return None
+            }
+
+            let mut head = head;
+            
+            let mut prev = head.as_mut();
+            while let Some(p) = prev {
+                if let Some(cur) = p.next.as_mut() {
+                    if p.val == cur.val {
+                        p.next = cur.next.take();
+                        prev = Some(p);
+                        continue
+                    }
+                }
+
+                prev = p.next.as_mut();
+            }
+
+            head
         }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::remove_duplicates_from_sorted_list::first_try::Solution;
     use crate::remove_duplicates_from_sorted_list::ListNode;
 
     #[test]
-    fn example() {
+    fn example_first_try() {
+        use crate::remove_duplicates_from_sorted_list::first_try::Solution;
+        assert_eq!(Solution::delete_duplicates(ListNode::generate(vec![])), ListNode::generate(vec![]));
+        assert_eq!(Solution::delete_duplicates(ListNode::generate(vec![1])), ListNode::generate(vec![1]));
+        assert_eq!(Solution::delete_duplicates(ListNode::generate(vec![1, 1, 1])), ListNode::generate(vec![1]));
+        assert_eq!(Solution::delete_duplicates(ListNode::generate(vec![1, 1, 2])), ListNode::generate(vec![1, 2]));
+        assert_eq!(Solution::delete_duplicates(ListNode::generate(vec![1, 1, 2, 3, 3])), ListNode::generate(vec![1, 2, 3]));
+    }
+
+    #[test]
+    fn example_second_try() {
+        use crate::remove_duplicates_from_sorted_list::second_try::Solution;
+        assert_eq!(Solution::delete_duplicates(ListNode::generate(vec![])), ListNode::generate(vec![]));
+        assert_eq!(Solution::delete_duplicates(ListNode::generate(vec![1])), ListNode::generate(vec![1]));
+        assert_eq!(Solution::delete_duplicates(ListNode::generate(vec![1, 1, 1])), ListNode::generate(vec![1]));
         assert_eq!(Solution::delete_duplicates(ListNode::generate(vec![1, 1, 2])), ListNode::generate(vec![1, 2]));
         assert_eq!(Solution::delete_duplicates(ListNode::generate(vec![1, 1, 2, 3, 3])), ListNode::generate(vec![1, 2, 3]));
     }
